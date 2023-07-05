716ba57a240a076b386d2ccffac1aa36a3fcdb13
vinicius <contato@viniccius.com.br>
2023-07-04 22:35:43 -0300

    fix http error && create GET

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,25 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// HTTP -> GET POST PUT DELETE
+
+app.get("/users", (request, response) => {
+  return response.json({
+    users: [
+      {
+        id: "01",
+        name: "josé deivid",
+        email: "deivid@only.com",
+      },
+      {
+        id: "02",
+        name: "luís oliviera",
+        email: "luisd@only.com",
+      },
+    ],
+  });
+});
+
 app.listen(port, () => {
-  console.log(`Server running in https://localhost:${port}`);
+  console.log(`Server running in http://localhost:${port}`);
 });
